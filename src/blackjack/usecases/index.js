2da35ae0e7f1f244a62decdd04abb6fc8dddc6bc
Carlos Calzadilla <maleras24@gmail.com>
2024-05-24 16:07:03 +0100

    First commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,8 @@
+
+// este arechico exporta todos los modules del programa y en el otro index.js solo requerimos una linea para esto 
+// se puede renombrar una importacion con as
+export { creaCartaHTML } from './crear-carta-html'
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
